431269414dff45b43f028e73b2a0a888ad64c48a
daramsai23 <dsai101112@gmail.com>
2026-07-15 00:03:18 -0400

    code for first instances created

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,13 @@
 resource "aws_launch_template" "My_First_Instance" {
-  
+  image_id      = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "My_First_Instance" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
+
   tags = {
     Name = "My-First-EC2"
-
   }
+}
